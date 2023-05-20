6fbe5d5c48b1eeaa4106e2eebfabaee0adcab8c1
cvp30 <carlos.valer.p30@gmail.com>
2023-05-20 16:47:55 -0500

    update db

--- api/src/database/database.js
+++ api/src/database/database.js
@@ -1,16 +1,21 @@
 import Sequelize from 'sequelize';
+const { DB_NAME, DB_USER, DB_PASSWORD, DB_HOST, DB_PORT } = process.env;
 
 // const sequelize = new Sequelize('database', 'username', 'password', {
 //   host: 'localhost',
 //   dialect: /* one of 'mysql' | 'postgres' | 'sqlite' | 'mariadb' | 'mssql' | 'db2' | 'snowflake' | 'oracle' */
 // });
 
+// TIPO_DB :// USER   :     PASSWORD        @ HOST      : PORT / DB_NAME
+// postgresql://postgres:iInLVkQfeKlOL0jcKb0C@containers-us-west-91.railway.app:7762/railway
+
 export const sequelize = new Sequelize(
-  "countries",
-  "postgres",
-  "1111",
+  DB_NAME,
+  DB_USER,
+  DB_PASSWORD,
   {
-    host: "localhost",
+    host: DB_HOST,
+    port: DB_PORT,
     dialect: "postgres",
     native: false,
     define: {
@@ -18,4 +23,13 @@ export const sequelize = new Sequelize(
       timestamps: false,
     }
   }
+  // {
+  //   host: "localhost",
+  //   dialect: "postgres",
+  //   native: false,
+  //   define: {
+  //     freezeTableName: true,
+  //     timestamps: false,
+  //   }
+  // }
 )
