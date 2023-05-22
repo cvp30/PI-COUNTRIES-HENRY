a86263c6c6bb46100182f90a38a53c48376488b4
cvp30 <carlos.valer.p30@gmail.com>
2023-05-21 23:55:55 -0500

    update db

---
+++ api/index.js
@@ -0,0 +1,25 @@
+const server = require('./src/app');
+const { sequelize } = require('./src/database/database');
+const { loadAllCountries } = require('./src/utils/index');
+require('./src/models/Activity');
+require('./src/models/Country');
+require('./src/models/CountryActivity');
+require("dotenv").config();
+const { PORT } = process.env;
+
+async function main() {
+  try {
+    await sequelize.sync({ force: true }).then(() => {
+
+      loadAllCountries();
+
+      server.listen(PORT, () => {
+        console.log('Server is listening on port %s', PORT);
+      });
+    })
+  } catch (error) {
+    console.log("unable to connet to the DB", error);
+  }
+}
+
+main();
